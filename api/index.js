de8c893372f6e1bb262355a7bc5a00e50ec8717f
Ralph Lee <dev.leeralph@gmail.com>
2024-11-26 03:15:16 +0800

    Added swagger.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
-const { sql } = require('@vercel/postgres');
 const express = require('express')
 const app = express();
+
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs')
+const YAML = require('yaml')
+
+const file  = fs.readFileSync('./api/swagger.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // enable middleware to parse body of Content-type: application/json
 app.use(express.json());
