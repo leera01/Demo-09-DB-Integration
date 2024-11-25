c07a58cbb3e93d2c7415741e0b37783962cadbee
Ralph Lee <dev.leeralph@gmail.com>
2024-11-26 03:59:10 +0800

    Rollbacl to 4.6.2

--- api/index.js
+++ api/index.js
@@ -8,9 +8,13 @@ const YAML = require('yaml')
 
 const file  =  fs.readFileSync(process.cwd() + '/swagger.yaml', 'utf8')
 const swaggerDocument = YAML.parse(file)
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui.min.css"
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, false, {customCss: CSS_URL}));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+    customCss:
+        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL,
+}));
 
 // enable middleware to parse body of Content-type: application/json
 app.use(express.json());
