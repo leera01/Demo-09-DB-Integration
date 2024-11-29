c8f28dd5b4d552cf4bdf4965f022de9bc74e736d
Ralph Lee <dev.leeralph@gmail.com>
2024-11-29 12:27:17 +0800

    Load openapi.yaml.

--- api/index.js
+++ api/index.js
@@ -1,20 +1,18 @@
 require('dotenv').config();
 const { sql } = require('@vercel/postgres');
-const express = require('express')
+const express = require('express');
 const app = express();
 
-const fs = require('fs')
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yaml')
+const fs = require('fs');
+const yamlFile = fs.readFileSync(process.cwd() + '/openapi.yaml', 'utf8');
 
-const file  =  fs.readFileSync(process.cwd() + '/openapi.yaml', 'utf8')
-const swaggerDocument = YAML.parse(file)
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+const YAML = require('yaml');
+const swaggerDocument = YAML.parse(yamlFile);
 
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
 	customCss:
 		'.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-	customCssUrl: CSS_URL,
+	customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css',
 }));
 
 // enable middleware to parse body of Content-type: application/json
